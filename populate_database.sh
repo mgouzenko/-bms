fbdd01de6c933d8cbdd6afb8b7f55731ae8a99d5
Mitchell Gouzenko <mgouzenko@gmail.com>
2016-03-04 15:00:29 -0500

    finalized test data

--- populate_database.sh
+++ populate_database.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# entrants
-psql -U mag2272 -h w4111a.eastus.cloudapp.azure.com proj1part2 \
-	-c "copy entrants from STDIN with delimiter as ','" < ./db/example_data/entrants.csv
+declare -a arr=("entrants" 					\
+				"admins" 					\
+				"service_employees"			\
+				"buildings"					\
+				"service_providers"			\
+				"works_for"					\
+				"administers"				\
+				"parking_spots"				\
+				"vehicles"					\
+				"of_a"						\
+				"provides_services_for"		\
+				"drives"					\
+				"units"						\
+				"unit_entrants"				\
+				"residents"					\
+				"casual_entrants"			\
+				"guests"					\
+				"owns")
 
-# admins
-psql -U mag2272 -h w4111a.eastus.cloudapp.azure.com proj1part2 \
-	-c "copy admins from STDIN with delimiter as ','" < ./db/example_data/admins.csv
-
-# service_employees
-psql -U mag2272 -h w4111a.eastus.cloudapp.azure.com proj1part2 \
-	-c "copy service_employees from STDIN with delimiter as ','" < ./db/example_data/service_employees.csv
-
-# buildings
-psql -U mag2272 -h w4111a.eastus.cloudapp.azure.com proj1part2 \
-	-c "copy buildings from STDIN with delimiter as ','" < ./db/example_data/buildings.csv
-
-# service_providers
-psql -U mag2272 -h w4111a.eastus.cloudapp.azure.com proj1part2 \
-	-c "copy service_providers from STDIN with delimiter as ','" < ./db/example_data/service_providers.csv
-
-# works_for
-psql -U mag2272 -h w4111a.eastus.cloudapp.azure.com proj1part2 \
-	-c "copy works_for from STDIN with delimiter as ','" < ./db/example_data/works_for.csv
-
-# administers
-psql -U mag2272 -h w4111a.eastus.cloudapp.azure.com proj1part2 \
-	-c "copy administers from STDIN with delimiter as ','" < ./db/example_data/administers.csv
+for table_name in "${arr[@]}"
+do
+	psql \
+		-U mag2272 \
+		-h w4111a.eastus.cloudapp.azure.com proj1part2 \
+		-c "copy $table_name from STDIN with delimiter as ','" \
+		< ./db/example_data/$table_name.csv
+done
