775df99640a334904693f423a1e3d0fddbbcc37f
Mitchell Gouzenko <mgouzenko@gmail.com>
2016-04-11 23:10:36 -0400

    added server. it connects to the db

---
+++ app/server.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python2.7
+
+"""
+Columbia's COMS W4111.001 Introduction to Databases
+Example Webserver
+
+To run locally:
+
+    python server.py
+
+Go to http://localhost:8111 in your browser.
+
+A debugger such as "pdb" may be helpful for debugging.
+Read about it online.
+"""
+
+import os
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask import Flask, request, render_template, g, redirect, Response
+
+tmpl_dir = os.path.join(
+    os.path.dirname(
+        os.path.abspath(__file__)),
+    'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+
+
+# The following is a dummy URI that does not connect to a valid database. You
+# will need to modify it to connect to your Part 2 database in order to use the
+# data.
+#
+# The URI should be in the format of:
+#
+#     postgresql://USER:PASSWORD@w4111a.eastus.cloudapp.azure.com/proj1part2
+
+DBURI = 'postgresql://mag2272:C9qlubhnlN@w4111a.eastus.cloudapp.azure.com/proj1part2'
+engine = create_engine(DBURI)
+
+
+@app.before_request
+def before_request():
+    """
+    This function is run at the beginning of every web request (every time you
+    enter an address in the web browser).  We use it to setup a database
+    connection that can be used throughout the request.
+
+    The variable g is globally accessible.
+    """
+    try:
+        g.conn = engine.connect()
+    except:
+        print "uh oh, problem connecting to database"
+        import traceback
+        traceback.print_exc()
+        g.conn = None
+
+
+@app.teardown_request
+def teardown_request(exception):
+    """
+    At the end of the web request, this makes sure to close the database
+    connection.  If you don't, the database could run out of memory!
+    """
+    try:
+        g.conn.close()
+    except Exception as e:
+        pass
+
+
+@app.route('/')
+def index():
+    """
+    request is a special object that Flask provides to access web request
+    information:
+
+    request.method:   "GET" or "POST" request.form:     if the browser submitted
+    a form, this contains the data in the form request.args:     dictionary of
+    URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
+
+    See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
+    """
+    return "hello world"
+
+
+@app.route('/another')
+def another():
+    return render_template("another.html")
+
+# Example of adding new data to the database
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form['name']
+    g.conn.execute('INSERT INTO test(name) VALUES (%s)', name)
+    return redirect('/')
+
+
+@app.route('/login')
+def login():
+    abort(401)
+    this_is_never_executed()
+
+if __name__ == "__main__":
+    import click
+
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+    def run(debug, threaded, host, port):
+        """
+        This function handles command line parameters.
+        Run the server using:
+
+            python server.py
+
+        Show the help text using:
+
+            python server.py --help
+
+        """
+
+        HOST, PORT = host, port
+        print "running on %s:%d" % (HOST, PORT)
+        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+    run()
